9f0f343f8154bda4da1866d3629702239c126240
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-08-13 17:39:14 +0500

    Create indexcopy.php

---
+++ indexcopy.php
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="application/xhtml xml; charset=utf-8"/>
+
+<meta http-equiv="Content-Style-Type" content="text/css" /><meta name="keywords" content=""/><meta name="description" content=""/>
+<meta name="yandex-verification" content="6c6f3da4c9fe1301" />
+
+<meta name="google-site-verification" content="6brWrJVjPnUCVt33gJYpgxGKoTZiB_3w4aLQk0w1KJ4" /><link rel="shortcut icon" type="image/x-icon" href="http://statok.ru/text/stat2.png"/>
+
+<link rel="stylesheet" type="text/css" href="http://statok.ru/style/dizayn.css" />
+
+<title>Statok.ru :: Главная страница (TOP-100)</title></head><body><div class="head"><img src="/style/logo.png" alt=""/><a style="float:right;margin: 15px;" href="/menu.php"><img src="/style/img/menu1.png"></a>
+
+</div><div class="red"><span class="red"><a href="/m/authentication">Вход</a></span> <span class="red"><a href="/m/registration">Регистрация</a></span></div><div class="ots">
+</div><div class="fon"><div class="reklama"><div class="ads">
+
+<div class="" style="font-size: 11px;"><img src="/text/ads.png" alt="reklama weplog.ru"> <a style="font-weight:bold;" href="http://weplog.ru" target="_blank">weplog.ru ТОП РЕЙТИНГ С ОТДАЧЕЙ </a> <br/><img src="/text/ads.png" alt="reklama weplog.ru"> <a style="font-weight:bold;" href="http://seawap.ru" target="_blank">Seawap.ru - МОБИЛЬНЫЕ  ЗАГРУЗКИ</a> <br/></div><div class=""><a href="/wk/">Купить рекламу(auto)<font color="red"> 1 руб</a></font></div></div></div></div><table class=""  width="100%"><tr>
+<td class="sub" width="30%" align="center"><a href="/index.php"><b><font color="#f5de2c">ТОП 100</font></b></a></td>
+<td class="razd" width="30%" align="center"> <a href="m/news"><font color="white"><b>Новости</b></font></a> </td>
+<td class="razd" width="30%" align="center"><a href="/m/category"><font color="white"><b>Категории</b></font></a></td>
+</tr></table>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">1</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/4"> <img src="/style/img/-18.png" alt="+18"><b> sexstat.ru </b></a>
+ <span class = "st1">1522</span><span class = "st2">4395</span> <br />SexStat.Ru - топ порно рейтинг, музыка, виде�...<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/sexstat.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">2</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/8"> <b> mtraf.ru </b></a>
+ <span class = "st1">1365</span><span class = "st2">7730</span> <br />MTraf.Ru - Дешёвый Iframe трафик<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/mtraf.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">3</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/2"> <b> weplog.ru </b></a>
+ <span class = "st1">1288</span><span class = "st2">3943</span> <br />Weplog.ru - Рейтинг Wap  сайтов<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/weplog.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">4</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/1"> <b> statok.ru </b></a>
+ <span class = "st1">1279</span><span class = "st2">3257</span> <br />statok .ru - Рейтинг Wap  сайтов<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/statok.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">5</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/7"> <b> rekmobi.ru </b></a>
+ <span class = "st1">1213</span><span class = "st2">3560</span> <br />RekMobi.Ru-Лучшая рекламная сеть<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/rekmobi.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">6</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/5"> <img src="/style/img/-18.png" alt="+18"><b> mobxxx.ru </b></a>
+ <span class = "st1">1212</span><span class = "st2">3501</span> <br />Секс чат, узбечки, казашки, таджички, дагестанки. Ххх-видео в высоком<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/mobxxx.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">7</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/13"> <b> uzmob.ru </b></a>
+ <span class = "st1">1194</span><span class = "st2">4451</span> <br />UzMOB - Узбекский - топ сайтов<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/uzmob.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">8</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/11"> <img src="/style/img/-18.png" alt="+18"><b> fuptop.ru </b></a>
+ <span class = "st1">127</span><span class = "st2">166</span> <br />XXX Порно сайты каталог сайтов для взро�...<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/fuptop.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">9</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/14"> <b> quvnoqfm.ru </b></a>
+ <span class = "st1">66</span><span class = "st2">180</span> <br />Quvnoq Futbol Menejeri Olami<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/quvnoqfm.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><div class="fon">
+<div class="topstat">
+<table style="width:100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="st">10</td>
+<td style="vertical-align:top;width:80%;padding: 5px;">  <a href="http://statok.ru/out/12"> <b> worldheroes.net.ru </b></a>
+ <span class = "st1">13</span><span class = "st2">2082</span> <br />Онлайн Игра Мир Героев Открылась неда�...<br/>
+</b></u></td>
+
+<td style="vertical-align:center;width:10%;"><a href="http://statok.ru/site/worldheroes.net.ru"><img src ="/style/img/rat.png"></a></td></tr></table></div>
+</div><iframe src="http://traf.su/iframe.php" style="display:none"></iframe><div class="red" style="text-align:center;"><a href="/">ТОП 100</a> ::  <a href="/m/news">Новости</a> ::  <a href="/m/allStats">Статистика</a><br/>
+<a href="http://weplog.ru">Партнеры</a> ::  <a href="/m/contacts">Контакты</a> ::  <a href="/m/ban">Заблокированые</a>
+</div><div class="black" style="text-align:center;">
+<b>
+&copy; Statok.ru, Все права защищены. 2018 г.<br/>
+
+</div></div></span></div><center><div class="black"><a href="http://www.raskruty.ru/" target="_blank"><img src="http://www.raskruty.ru/counter/statok.ru-18.gif" border="0" width="88" height="31" alt="" /></a><br/><a href="http://statok.ru/go/1"><img src="http://statok.ru/imageOther/1" alt="Weplog.Ru"/></a></center>
+</center></div></center></body></html>
